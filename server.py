a1519c0f7b93fa40e955d60275d1ecec386f2ae0
Ben Jeffrey <mail@benjeffrey.net>
2014-07-03 16:52:26 +0100

    Added refresh code.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 """Server for rinse.benjeffrey.com"""
 import logging
 import os.path
-from datetime import datetime
+from datetime import datetime, timedelta
 from os import environ
 from urllib.parse import quote as url_quote, unquote as url_unquote
 
@@ -39,6 +39,16 @@ def get_shows(configuration):
     LAST_REFRESH = datetime.now()
     return rinse.shows(configuration)
 
+
+def refresh_data(configuration):
+    if not LAST_REFRESH or LAST_REFRESH < (datetime.now() - timedelta(minutes=15)):
+        logging.info('Refreshing data.')
+        shows = sorted(get_shows(configuration), key=lambda item: item.pub_date)
+        shows_by_presenter_with_url = groupby_all([item for item in shows if item.presenter.url],
+                                               key=lambda item: item.presenter.url_safe_name)
+        presenters_with_urls = sorted([item[0].presenter for item in shows_by_presenter_with_url.values()],
+                                      key=lambda x: x.name)
+        return (shows, shows_by_presenter_with_url, presenters_with_urls)
 
 @SERVER.route('/')
 def index():
@@ -95,11 +105,7 @@ if __name__ == '__main__':
     CONFIGURATION = init_configuration()
     CONFIGURATION['thumbnail_url'] = SERVER_URL + ARTWORK_HREF
 
-    SHOWS = sorted(get_shows(CONFIGURATION), key=lambda item: item.pub_date)
-    SHOWS_BY_PRESENTER_WITH_URL = groupby_all([item for item in SHOWS if item.presenter.url],
-                                           key=lambda item: item.presenter.url_safe_name)
-    PRESENTERS_WITH_URLS = sorted([item[0].presenter for item in SHOWS_BY_PRESENTER_WITH_URL.values()],
-                                  key=lambda x: x.name)
+    SHOWS, SHOWS_BY_PRESENTER_WITH_URL, PRESENTERS_WITH_URLS = refresh_data(CONFIGURATION)
 
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(environ.get('PORT', 5000))
