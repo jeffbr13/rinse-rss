812de5a796d80cc6cee18044d53019bccfa9d0d5
Ben Jeffrey <mail@benjeffrey.net>
2017-09-21 17:55:57 +0100

    Don't require DEBUG envvar to be set.

--- rinse_rss/rinse_rss/settings.py
+++ rinse_rss/rinse_rss/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=list()) + ['localhost']
 
