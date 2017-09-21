e5a92ccfcd088c946a959c12b6d9bca17c322e26
Ben Jeffrey <mail@benjeffrey.net>
2017-09-21 20:15:59 +0100

    Podcast audio can have larger content-length.

--- rinse_rss/rinsefm/models.py
+++ rinse_rss/rinsefm/models.py
@@ -6,7 +6,7 @@ class PodcastEpisode(models.Model):
     title = models.CharField(max_length=200)
     broadcast_date = models.DateTimeField()
     audio_url = models.URLField(unique=True)
-    audio_content_length = models.PositiveSmallIntegerField()
+    audio_content_length = models.PositiveIntegerField()
     audio_content_type = models.CharField(max_length=200)
 
     def __str__(self):
