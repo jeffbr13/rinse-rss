2561448904716c2ed3b42752dd36fa4e7f0c620c
Ben Jeffrey <mail@benjeffrey.net>
2017-11-01 17:54:09 +0000

    Set podcast feed attributes by class attrs.

--- rinse_rss/rinsefm/views.py
+++ rinse_rss/rinsefm/views.py
@@ -11,7 +11,21 @@ def index(request: HttpRequest):
     return render(request, 'index.html')
 
 
-class PodcastFeed(Rss201rev2Feed):
+class ItunesPodcastRssFeed(Rss201rev2Feed):
+    def __init__(self, title, link, description, language=None, author_email=None,
+                 author_name=None, author_link=None, subtitle=None, categories=None,
+                 feed_url=None, feed_copyright=None, feed_guid=None, ttl=None,
+                 artwork_link=None, category=None, explicit=None, owner_name=None, owner_email=None,
+                 **kwargs):
+        super().__init__(title, link, description, language, author_email,
+                         author_name, author_link, subtitle, categories,
+                         feed_url, feed_copyright, feed_guid, ttl, **kwargs)
+        self.feed['artwork_link'] = artwork_link
+        self.feed['itunes_category'] = category
+        self.feed['explicit'] = explicit
+        self.feed['owner_name'] = owner_name
+        self.feed['owner_email'] = owner_email
+
     def root_attributes(self):
         attrs = super().root_attributes()
         attrs['xmlns:itunes'] = 'http://www.itunes.com/dtds/podcast-1.0.dtd'
@@ -19,14 +33,14 @@ class PodcastFeed(Rss201rev2Feed):
 
     def add_root_elements(self, handler):
         super().add_root_elements(handler)
-        handler.addQuickElement('itunes:image', attrs={'href': self.feed.get('artwork_url', '')})
+        handler.addQuickElement('itunes:image', attrs={'href': self.feed.get('artwork_link', '')})
         handler.addQuickElement('itunes:subtitle', self.feed.get('subtitle', ''))
         handler.addQuickElement('itunes:summary', self.feed.get('description', ''))
-        handler.addQuickElement('itunes:category', attrs={'text': self.feed.get('category', '')})
+        handler.addQuickElement('itunes:category', attrs={'text': self.feed.get('itunes_category', '')})
         handler.addQuickElement('itunes:keywords', ', '.join(self.feed.get('categories', [])))
         handler.addQuickElement('itunes:explicit', 'yes' if self.feed.get('explicit') else 'clean')
-
         handler.addQuickElement('itunes:author', self.feed.get('author_name'))
+
         handler.startElement('itunes:owner', {})
         handler.addQuickElement('itunes:name', self.feed.get('owner_name'))
         handler.addQuickElement('itunes:email', self.feed.get('owner_email'))
@@ -34,13 +48,28 @@ class PodcastFeed(Rss201rev2Feed):
 
     def add_item_elements(self, handler, item):
         super().add_item_elements(handler, item)
-        handler.addQuickElement('itunes:summary', item.get('description'))
-        handler.addQuickElement('itunes:duration', item.get('duration'))
-        handler.addQuickElement('itunes:author', item.get('author_name'))
+        if item.get('description'):
+            handler.addQuickElement('itunes:summary', item.get('description'))
+        if item.get('duration'):
+            handler.addQuickElement('itunes:duration', item.get('duration'))
+        if item.get('author_name'):
+            handler.addQuickElement('itunes:author', item.get('author_name'))
 
 
-class AllPodcastsFeed(Feed):
-    feed_type = PodcastFeed
+class PodcastFeed(Feed):
+    feed_type = ItunesPodcastRssFeed
+
+    def get_feed(self, obj, request):
+        feed = super().get_feed(obj, request)
+        feed.feed['artwork_link'] = self._get_dynamic_attr('artwork_link', obj)
+        feed.feed['itunes_category'] = self._get_dynamic_attr('itunes_category', obj)
+        feed.feed['explicit'] = self._get_dynamic_attr('explicit', obj)
+        feed.feed['owner_name'] = self._get_dynamic_attr('owner_name', obj)
+        feed.feed['owner_email'] = self._get_dynamic_attr('owner_email', obj)
+        return feed
+
+
+class AllPodcastsFeed(PodcastFeed):
     link = 'http://rinse.fm/'
     title = 'Rinse FM'
     subtitle = "London's favourite pirate radio station"
@@ -58,20 +87,15 @@ class AllPodcastsFeed(Feed):
         "Rinseʼs fiercely grass-roots broadcasting ethos engages massively under-represented communities, "
         "especially 15-24 year olds"
     )
-    category = 'Music'
     categories = ('Rinse FM', 'rinse', 'grime', 'garage', 'dubstep', 'house', 'deep house', 'techno', 'urban', 'music')
+    # iTunes podcast attributes
+    artwork_link = static('artwork.png')
+    itunes_category = 'Music'
     explicit = True
     author_name = 'Rinse RSS'
     copyright = 'Rinse RSS'
-
-    def feed_extra_kwargs(self, obj):
-        return {
-            'category': self.category,
-            'explicit': True,
-            'artwork_url': static('artwork.png'),
-            'owner_name': 'Ben Jeffrey',
-            'owner_email': 'br-rss@jeffbr13.net',
-        }
+    owner_name = 'Ben Jeffrey'
+    owner_email = 'br-rss@jeffbr13.net'
 
     def items(self):
         return PodcastEpisode.objects.all()
